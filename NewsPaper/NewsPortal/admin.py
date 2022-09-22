573e3e8ae875d0701962c0d157b1826057a310b3
nikita <ogyrtsov.nikita@gmail.com>
2022-09-22 14:32:50 +0300

    D14.1

--- NewsPaper/NewsPortal/admin.py
+++ NewsPaper/NewsPortal/admin.py
@@ -3,6 +3,29 @@ from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment, CategorySubscribers
 
 
+def like_plus_five(modeladmin, request, queryset):
+    # функция накрутки лайков статье
+    for posts in queryset:
+        posts.rating = posts.rating + 5
+        posts.save()
+
+
+def like_minus_five(modeladmin, request, queryset):
+    # функция  скрутки лайков статье
+    for posts in queryset:
+        posts.rating = posts.rating - 5
+        posts.save()
+
+
+# создаём новый класс для представления товаров в админке
+class PostAdmin(admin.ModelAdmin):
+    # list_display - это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('id', 'post_title', 'position', 'who_author', 'post_rating',)
+    list_filter = ('category_post', 'who_author')  # добавляем примитивные фильтры в нашу админку
+    search_fields = ('post_title',)  # тут всё очень похоже на фильтры из запросов в базу
+    actions = [like_plus_five, like_minus_five]  # добавляем действия в список
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'category_name')
 
@@ -12,9 +35,9 @@ class CategorySubscribersAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(CategorySubscribers)
-admin.site.register(Category)
+admin.site.register(CategorySubscribers, CategorySubscribersAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
